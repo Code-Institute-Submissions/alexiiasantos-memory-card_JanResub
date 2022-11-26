3e83a5d2e9935f3cf500fe62569a12b31bbc485c
Alexia <salexxndra@gmail.com>
2022-11-26 14:56:34 +0000

    Add unflippedcards and resetboard function

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 const cards = document.querySelectorAll('.card');
 
+let hasFlippedCard = false;
+let lockBoard = false;
+let firstCard, secondCard;
+
 function flipCard() {
     if (lockBoard) return;
     if (this === firstCard) return;
@@ -31,6 +35,22 @@ function checkForMatch() {
     secondCard.removeEventListener('click', flipCard);
   
     resetBoard();
+  }
+
+  function unflipCards() {
+    lockBoard = true;
+  
+    setTimeout(() => {
+      firstCard.classList.remove('flip');
+      secondCard.classList.remove('flip');
+  
+      resetBoard();
+    }, 1500);
+  }
+  
+  function resetBoard() {
+    [hasFlippedCard, lockBoard] = [false, false];
+    [firstCard, secondCard] = [null, null];
   }
 
 cards.forEach(card => card.addEventListener('click', flipCard));
