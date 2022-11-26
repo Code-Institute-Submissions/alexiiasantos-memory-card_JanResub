fd2d1c5413a09be575c091d6425f3a0b29449e08
Alexia <salexxndra@gmail.com>
2022-11-26 14:51:47 +0000

    Add checkformatch function

--- script.js
+++ script.js
@@ -1,7 +1,36 @@
 const cards = document.querySelectorAll('.card');
 
 function flipCard() {
-    this.classList.toggle("flip");
+    if (lockBoard) return;
+    if (this === firstCard) return;
+  
+    this.classList.add('flip');
+  
+    if (!hasFlippedCard) {
+      // first click
+      hasFlippedCard = true;
+      firstCard = this;
+  
+      return;
+    }
+
+      // second click
+  secondCard = this;
+
+  checkForMatch();
+}
+
+function checkForMatch() {
+    let isMatch = firstCard.dataset.framework === secondCard.dataset.framework;
+  
+    isMatch ? disableCards() : unflipCards();
+  }
+  
+  function disableCards() {
+    firstCard.removeEventListener('click', flipCard);
+    secondCard.removeEventListener('click', flipCard);
+  
+    resetBoard();
   }
 
 cards.forEach(card => card.addEventListener('click', flipCard));
