1bbf51ef107de54ffaeb2c4d6da5faeffd41a1c1
Alexia <salexxndra@gmail.com>
2022-11-26 15:23:01 +0000

    Add correct data framework

--- script.js
+++ script.js
@@ -60,4 +60,22 @@ function checkForMatch() {
     });
   })();
 
+  if (document.readyState == 'loading') {
+    document.addEventListener('DOMContentLoaded', ready);
+} else {
+    ready();
+}
+
+function ready() {
+    let overlays = Array.from(document.getElementsByClassName('overlay-text'));
+
+    overlays.forEach(overlay => {
+        overlay.addEventListener('click', () => {
+            overlay.classList.remove('visible');
+            game.startGame();
+        });
+    });
+
+}
+
 cards.forEach(card => card.addEventListener('click', flipCard));
